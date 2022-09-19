9c15da359c7783dedf8eb387f2fbbc4f0afb197a
vibek lamichhane <vibek@copenned.com>
2022-09-19 12:49:49 +0545

    add home page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fc5c077decde64d5"; }
+/******/ 		__webpack_require__.h = function() { return "5f14a2fac239a08c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { Col, Row } from "react-bootstrap";
 import ExploreServicesCard from "../components/ExploreServicesCard";
+import BannerComponent from "../components/Home/BannerComponent";
 import Navbar from "../components/Navbar";
 
 
@@ -9,14 +10,7 @@ export default function Home() {
     <div>
       <div className='sm:text-xs md:text-9xl '>
        <Navbar/>
-       <Row style={{marginTop:'15vh'}} className='d-flex flex-wrap'>
-<Col md={4}><ExploreServicesCard/></Col>
-<Col md={4}><ExploreServicesCard/></Col>
-<Col md={4}><ExploreServicesCard/></Col>
-<Col className='mt-4' md={4}><ExploreServicesCard/></Col>
-<Col className='mt-4' md={4}><ExploreServicesCard/></Col>
-<Col className='mt-4' md={4}><ExploreServicesCard/></Col>
-       </Row>
+       <BannerComponent/>
        
       </div>
     </div>
